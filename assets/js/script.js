a12755e11b1565a6abfe4be4447fbafd1dea22e9
Makaosva <maka.osses@gmail.com>
2024-03-30 09:55:24 -0300

    feat/arreglo funciones de aumento y disminucion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-precio = 400000
+precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+
+total = 1;
+/*funcion para aumentar cantidad*/
+function aumentaClick() {
+  cantidadSpan = document.querySelector(".cantidad");
+  aumentar = Number(cantidadSpan.innerHTML);
+  aumentar++;
+  cantidadSpan.innerHTML = aumentar;
+  pagarSpan = document.querySelector(".valor-total");
+  pagarSpan.innerHTML = precio * aumentar;
+}
+/*funcion para disminuir cantidad*/
+function diminuyeClick() {
+  cantidadSpan = document.querySelector(".cantidad");
+  disminuir = Number(cantidadSpan.innerHTML);
+  disminuir--;
+  cantidadSpan.innerHTML = disminuir;
+  pagarSpan = document.querySelector(".valor-total");
+  pagarSpan.innerHTML = precio * disminuir;
+}
